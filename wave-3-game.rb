8532949538dc3d866708479864739f65023d840c
dee <dee@adadevelopersacademy.org>
2017-08-24 17:06:13 -0700

    conform formatting to use parens

--- wave-3-game.rb
+++ wave-3-game.rb
@@ -15,15 +15,15 @@ module Scrabble
         @players.each do |player|
           puts "It is #{player.name}'s turn"
 
-          player.draw_tiles @tilebag
+          player.draw_tiles(@tilebag)
 
           puts "#{player.name} has the following tiles: #{player.tiles}"
 
-          player_word = get_word_for player
+          player_word = get_word_for(player)
           player_has_won = !player_word
 
           if player_word
-            print_score player_word
+            print_score(player_word)
           end
 
           if player_has_won
@@ -43,7 +43,7 @@ module Scrabble
 
       players = []
       num_of_players.times do |player|
-        players << Player.new(get_player_name player)
+        players << Player.new(get_player_name(player))
       end
       return players
     end
@@ -56,7 +56,7 @@ module Scrabble
       return true if @words.length == 0 # haven't started playing yet
       @players.each do |player|
         if player.won?
-          crown_winner player
+          crown_winner(player)
           return false
         end
       end
@@ -66,12 +66,12 @@ module Scrabble
       (continue == "Y") ? true : false
     end
 
-    def get_word_for player
+    def get_word_for(player)
       puts "Enter a word to score:"
       word = gets.chomp
       @words << word
 
-      keep_playing = player.play word
+      keep_playing = player.play(word)
 
       if keep_playing
         return word
@@ -85,7 +85,7 @@ module Scrabble
       puts "The score of #{ word } is #{ result }"
     end
 
-    def crown_winner player
+    def crown_winner(player)
       puts "#{player.name} has won!"
     end
 
@@ -94,7 +94,7 @@ module Scrabble
       puts "The final highest scoring word for all players is #{ highest_word }"
 
       @players.each do |player|
-        puts "The highest scoring word for #{player.name} is #{player.highest_scoring_word} for #{player.highest_word_score}"
+        puts "The highest scoring word for #{player.name} is \"#{player.highest_scoring_word}\" for #{player.highest_word_score} points"
       end
     end
 
@@ -107,7 +107,7 @@ module Scrabble
       return input.to_i
     end
 
-    def get_player_name player_number
+    def get_player_name(player_number)
       puts "What is the name of Player \##{player_number + 1}?"
       return gets.chomp
     end
