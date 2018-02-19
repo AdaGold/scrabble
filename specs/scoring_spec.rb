a7e842bc65c2968794ae2872d6f435e148e55a1e
Dan Roberts <dan@adadevelopersacademy.org>
2018-02-18 21:11:11 -0800

    Copy tests from js-scrabble project, and convert to minitest syntax

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -8,5 +8,55 @@ require_relative '../lib/scoring'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Scoring' do
-  
+  describe 'score' do
+    it 'correctly scores simple words' do
+      Scrabble::Scoring.score('dog').must_equal 5
+      Scrabble::Scoring.score('cat').must_equal 5
+      Scrabble::Scoring.score('pig').must_equal 6
+    end
+
+    it 'adds 50 points for a 7-letter word' do
+      Scrabble::Scoring.score('academy').must_equal 65
+    end
+
+    it 'handles all upper- and lower-case letters' do
+      Scrabble::Scoring.score('dog').must_equal 5
+      Scrabble::Scoring.score('DOG').must_equal 5
+      Scrabble::Scoring.score('DoG').must_equal 5
+    end
+
+    it 'returns nil for strings containing bad characters' do
+      Scrabble::Scoring.score('#$%^').must_be_nil
+      Scrabble::Scoring.score('char^').must_be_nil
+      Scrabble::Scoring.score(' ').must_be_nil
+    end
+
+    it 'returns nil for words > 7 letters' do
+      Scrabble::Scoring.score('abcdefgh').must_be_nil
+    end
+
+    it 'returns nil for empty words' do
+      Scrabble::Scoring.score('').must_be_nil
+    end
+  end
+
+  describe 'highest_score_from' do
+    it 'returns nil if no words were passed' do
+    end
+
+    it 'returns the only word in a length-1 array' do
+    end
+
+    it 'returns the highest word if there are two words' do
+    end
+
+    it 'if tied, prefer a word with 7 letters' do
+    end
+
+    it 'if tied and no word has 7 letters, prefers the word with fewer letters' do
+    end
+
+    it 'returns the first word of a tie with same letter count' do
+    end
+  end
 end
