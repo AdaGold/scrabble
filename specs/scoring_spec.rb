7a1d0fdcf0a9a31be16864bf336af16034174465
Dan Roberts <dan@adadevelopersacademy.org>
2018-02-18 21:10:29 -0800

    Initial testing setup

---
+++ specs/scoring_spec.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
+
+require_relative '../lib/scoring'
+
+# Get that nice colorized output
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+describe 'Scoring' do
+  
+end
