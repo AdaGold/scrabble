27884635c871b6ce0a2c67980e2b625f6b39c8dc
CheezItMan <chris@adadevelopersacademy.org>
2018-07-09 11:36:54 -0700

    update scoring_spec.rb to expect() syntax

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -10,33 +10,33 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 describe 'Scoring' do
   describe 'score' do
     it 'correctly scores simple words' do
-      Scrabble::Scoring.score('dog').must_equal 5
-      Scrabble::Scoring.score('cat').must_equal 5
-      Scrabble::Scoring.score('pig').must_equal 6
+      expect(Scrabble::Scoring.score('dog')).must_equal 5
+      expect(Scrabble::Scoring.score('cat')).must_equal 5
+      expect(Scrabble::Scoring.score('pig')).must_equal 6
     end
 
     it 'adds 50 points for a 7-letter word' do
-      Scrabble::Scoring.score('academy').must_equal 65
+      expect(Scrabble::Scoring.score('academy')).must_equal 65
     end
 
     it 'handles all upper- and lower-case letters' do
-      Scrabble::Scoring.score('dog').must_equal 5
-      Scrabble::Scoring.score('DOG').must_equal 5
-      Scrabble::Scoring.score('DoG').must_equal 5
+      expect(Scrabble::Scoring.score('dog')).must_equal 5
+      expect(Scrabble::Scoring.score('DOG')).must_equal 5
+      expect(Scrabble::Scoring.score('DoG')).must_equal 5
     end
 
     it 'returns nil for strings containing bad characters' do
-      Scrabble::Scoring.score('#$%^').must_be_nil
-      Scrabble::Scoring.score('char^').must_be_nil
-      Scrabble::Scoring.score(' ').must_be_nil
+      expect(Scrabble::Scoring.score('#$%^')).must_be_nil
+      expect(Scrabble::Scoring.score('char^')).must_be_nil
+      expect(Scrabble::Scoring.score(' ')).must_be_nil
     end
 
     it 'returns nil for words > 7 letters' do
-      Scrabble::Scoring.score('abcdefgh').must_be_nil
+      expect(Scrabble::Scoring.score('abcdefgh')).must_be_nil
     end
 
     it 'returns nil for empty words' do
-      Scrabble::Scoring.score('').must_be_nil
+      expect(Scrabble::Scoring.score('')).must_be_nil
     end
   end
 
